c8a3a0a633fae220cdf30598b487bad8d47ba268
noorex95 <44781341+noorex95@users.noreply.github.com>
2018-11-15 01:07:22 +0200

    Create parents.js

---
+++ home task 3 - Family tree/parents.js
@@ -0,0 +1,57 @@
+var parentsData = {
+name: 'Oleg',
+status: 'son',
+parents:[
+
+	  {name: 'Igor',
+	  status: 'dad',
+  	parents:[
+		
+		        {name: 'Victor',
+	         	status: 'grandfather',
+	        	parents:[
+				
+			              	{name: 'Pavel',
+			              	status: 'great grandfather'}, 
+
+			              	{name: 'Ekaterina',
+			              	status: 'great grandmother'}]
+		                	},
+
+        		{name: 'Oksana',
+        		status: 'grandmother',
+	        	parents:[
+
+		          		    {name: 'Alexey',
+		          	    	status: 'great grandfather'}, 
+
+			              	{name: 'Valeria',
+			              	status: 'great grandmother'}]
+		            	}]},
+
+
+   {name: 'Marina',
+    status: 'Mom',
+	  parents:[
+
+		        {name: 'Sergey',
+        		status: 'grandfather',
+	        	parents:[
+
+			              	{name: 'Vladimir',
+			              	status: 'great grandfather'},
+
+			              	{name: 'Yana',
+		          	    	status: 'great grandmother'}]},
+
+        		{name: 'Olga',
+         		status: 'grandmother',
+         		parents:[
+			
+			              	{name: 'Denis',
+		           	    	status: 'great grandfather'},
+			
+		              		{name: 'Irina',
+			               	status: 'great grandmother'}]}]}
+
+      ]}
